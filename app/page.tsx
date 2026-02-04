a8e85c1ca222a505040522b8ad87e9ee7fc3be18
riyaawal <reeyaawal123@gmail.com>
2026-02-04 14:10:30 +0545

    Added admin panel & Herosection

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
+import Navbar from "@/components/NavBar";
+import Hero from "./home/page";
+
+
 export default function HomePage() {
   return (
-    <main className="p-10">
-      <h1 className="text-4xl font-bold">Welcome to ShopX</h1>
-      <p className="mt-4 text-gray-600">
-        Discover quality products at affordable prices.
-      </p>
-      <button className="mt-6 px-6 py-3 bg-black text-white rounded">
-        Shop Now
-      </button>
-    </main>
+    <>
+      <Navbar />
+      <Hero />
+    </>
   )
 }
